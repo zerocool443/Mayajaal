ed9c957065e4b135b39d94a4922415cb2e294bf8
Akash <skyatwork007@gmail.com>
2017-03-21 11:21:32 +0530

    sample_boiler_pplate

---
+++ sample_scan_log.py
@@ -0,0 +1,47 @@
+import shodan
+import sys
+key="X8jGgSyWwH5AqN2tFKybmicNeoBgRMXk"
+api=shodan.Shodan(key);
+
+if __name__ == '__main__':
+	try:
+		q=sys.argv[1];
+		print "Scanning .........."
+		results = api.search(q);
+		print 'Results Found : '
+		print len(results['matches'])
+		for result in results['matches']:
+
+			print """
+                IP: %s
+                HOSTNAME: %s
+                OS: %s
+                PORT: %s
+
+        			""" % (result['ip_str'], result['hostnames'],result['os'],result['port'])
+		fname=str(q)+"_results"
+		ff=open(fname,'w')
+		ff.write(results['matches'])
+
+	except shodan.APIError, e:
+		print 'Error: %s' % e
+else:
+	q=raw_input("Please provide me your query   ");
+	try:
+		results = api.search(q);
+		print 'Results Found : '
+		print len(results['matches'])
+		for result in results['matches']:
+
+			print """
+                IP: %s
+                HOSTNAME: %s
+                OS: %s
+                PORT: %s
+
+        		""" % (result['ip_str'], result['hostnames'],result['os'],result['port'])
+
+	except shodan.APIError, e:
+			print 'Error: %s' % e
+
+
